b9c41ce28e51d3d6c19258382b08fb46b4d0f343
levprost <smolev17@gmail.com>
2024-08-17 03:14:33 +0200

    bfg
    vcb

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -3,7 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Post;
-use App\Entity\Step;
+use App\Entity\User;
 use App\Repository\PostRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -23,8 +23,8 @@ class PostController extends AbstractController
     #[Route('/', name: 'app_post')]
     public function index(): Response
     {
-        $posts = $this->repo->findBy([],['createdAt'=>'DESC'],1);
-        $posts2 = $this->repo->findBy([],['createdAt' => 'DESC'],4,1);
+        $posts = $this->repo->findBy([],[],1);
+        $posts2 = $this->repo->findBy([],[],4,1);
 
         return $this->render('post/index.html.twig',[
             'posts' => $posts, 'posts2' => $posts2,
@@ -32,17 +32,15 @@ class PostController extends AbstractController
     }
 
     #[Route('/post/{id}', name: 'show', requirements: ['id' => '\d+'])]
-    public function showone(Post $post, Step $steps ,$id, PostRepository $reppo, EntityManagerInterface $emi): Response
+    public function showone(Post $post,$id, PostRepository $reppo, EntityManagerInterface $emi): Response
     {
         // Vérification du post
         if (!$post) {
             return $this->redirectToRoute('app_post');
         }
-        $steps = $post->getStep()->toArray();
         $posts = $reppo->find($id);
         return $this->render('show/show.html.twig', [
-            'post' => $post,
-            'step' => $steps
+            'post' => $post
         ]);
     }
 }
